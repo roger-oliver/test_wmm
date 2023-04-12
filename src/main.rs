f46ffce04f6f5ef2bb007bba6592d295ccfc9785
Rogerio Oliveira <roliveira@cyclomedia.com>
2023-04-13 01:23:43 +0200

    improved to read a list of points;

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,43 @@
-use time::{OffsetDateTime, Date};
+use geo::Point;
+use time::{Date, Month};
+use wkt::TryFromWkt;
 use wmm::declination;
 
 fn main() {
-    let date = Date::from_calendar_date(2023, time::Month::February, 9).unwrap();  // OffsetDateTime::now_utc().date();
-    let lat = 5.251070246;
-    let lon = 51.79475163;
-    let dec = declination(date, lat, lon).unwrap();
-
-    println!(
-        "Today's declination for coordinates {},{} is {}°",
-        lat, lon, dec
-    );
-
+    let date = Date::from_calendar_date(2023, Month::February, 9).unwrap();
     let true_north = 0.0; // assuming that true north is at 0 degrees
-    let orientation = (lon - true_north - dec.to_degrees()).to_degrees() / 360.0;
 
-    println!("Orientation relative to true north: {:.2} degrees", orientation);
+    let coords = vec![
+        "POINT (5.250744062 51.794809988)",
+        "POINT (5.250804877 51.794825014)",
+        "POINT (5.25087085 51.794806756)",
+        "POINT (5.25093729 51.794788589)",
+        "POINT (5.251003619 51.794770417)",
+        "POINT (5.251070246 51.79475163)",
+        "POINT (5.251136548 51.794733338)",
+        "POINT (5.251202792 51.794715031)",
+        "POINT (5.251269206 51.794696656)",
+        "POINT (5.251335485 51.79467775)",
+        "POINT (5.251401806 51.794659382)",
+        "POINT (5.25146818 51.794641061)",
+    ];
 
+    for coord in coords {
+        let point: Point<f32> = Point::try_from_wkt_str(&coord).unwrap();
+    
+        let lon = point.x();
+        let lat = point.y();
+
+        let dec = declination(date, lat, lon).unwrap();
+        let orientation = (lon - true_north - dec.to_degrees()).to_degrees() / 360.0;
+
+        println!(
+            "Magnetic declination for coordinates {},{} is {:.2}°",
+            lat, lon, dec.to_degrees()
+        );
+        println!(
+            "Orientation relative to true north for coordinates {},{} is {:.2} degrees",
+            lat, lon, orientation * 360.0
+        );
+    }
 }
